7c0ae0c4783807a3acbba67d58839c4f8da37b24
NehaGitRepo <93670405+NehaGitRepo@users.noreply.github.com>
2025-07-14 16:16:40 +0530

    first update

--- 3HandlingDropDowns.py
+++ 3HandlingDropDowns.py
@@ -22,3 +22,25 @@ for option in DropDownOptions:
 
 #printing total number of languages
 print("Total Drop Down values are: ", len(DropDownOptions))
+
+#Printing all the links in webpage
+links = driver.find_elements(By.TAG_NAME,"a")
+print("Total number of links are: ", len(links))
+for link in links:
+    print("Text is: ",link.text, " | URL is: ", link.get_attribute("href"))
+#printing only 4th link text
+print("4th link in page is:" ,driver.find_elements(By.TAG_NAME,"a").__getitem__(4).text)
+
+#printing links from a particular section of page
+print("-----------------Printing links in a block-----------------------------")
+block =driver.find_element(By.XPATH,"//nav[@aria-label='Other projects']")
+links=block.find_elements(By.TAG_NAME,"a")
+print("Total number of links in block are: ", len(links))
+
+print("----first link is---: ",driver.find_elements(By.TAG_NAME,"a").__getitem__(1).text)
+
+for link in links:
+    print("Text is: ",link.text, " | URL is: ", link.get_attribute("href"))
+
+
+print("1st link in block is:" ,block.find_elements(By.TAG_NAME,"a").__getitem__(0).text)
